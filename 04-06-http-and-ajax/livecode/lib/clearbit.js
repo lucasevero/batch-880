8c70eb3ff9789d36b27590443b62cf3cd578ef63
Luca Severo <luca98.severo@gmail.com>
2022-05-11 20:04:20 +0200

    Added Vincent debugging sugestion

--- 04-06-http-and-ajax/livecode/lib/clearbit.js
+++ 04-06-http-and-ajax/livecode/lib/clearbit.js
@@ -27,3 +27,51 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   callClearbitApi(fillTheInTheBlanks);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 👇 --- don't go further if you are not super confident with js --- 👇
+
+// ALTERNATIVE REFACTOR USING async AND await
+// See more on: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
+// Super shoutout to Vincent for sugesting this code!
+
+
+
+// const fetchClearbitApi = (input) => {
+//   const url = `https://person.clearbit.com/v1/people/email/${input}`
+
+//   return fetch(url, {
+//     headers: { 'Authorization': authorization }
+//   })
+//     .then(response => response.json())
+// };
+
+// const fillTheInTheBlanks = (data) => {
+//   const name = data.name.fullName;
+//   const email = data.email;
+//   const bio = data.bio;
+//   const location = data.geo.city;
+
+//   document.getElementById('userName').innerText = name;
+//   document.getElementById('userEmail').innerText = email;
+//   document.getElementById('userBio').innerText = bio;
+//   document.getElementById('userLocation').innerText = location;
+// }
+
+// // form.addEventListener(EVENT, CALLBACK)
+// form.addEventListener('submit', async () => {
+//   const userInput = document.getElementById('clearbitEmail').value
+//   const data = await fetchClearbitApi(userInput);
+//   fillTheInTheBlanks(data)
+// })
