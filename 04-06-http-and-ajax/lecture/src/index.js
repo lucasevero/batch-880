fdd9c895a4625b9f6c54f1f6df8c0f8788bcf1a6
Luca Severo <luca98.severo@gmail.com>
2022-05-11 10:28:44 +0200

    HTTP and AJAX lecture done!

--- 04-06-http-and-ajax/lecture/src/index.js
+++ 04-06-http-and-ajax/lecture/src/index.js
@@ -1,1 +1,52 @@
-console.log("Hello from src/index.js!");
+// TO REFRESH YOUR MEMORY
+// // const button = document.querySelector('#click-me')
+// const button = document.getElementById('click-me')
+
+// // button.addEventListener(EVENT_TYPE, CALLBACK)
+// button.addEventListener('click', (event) => {
+//   button.innerText = "Hold on...";
+//   // button.disabled = true;
+//   button.setAttribute("disabled", "")
+// });
+
+// FILMS DEMO
+const form = document.getElementById('search-form');
+const results = document.getElementById('results');
+const input = document.getElementById('search-input');
+
+form.addEventListener('submit', (event) => {
+  console.log(input);
+  event.preventDefault();
+  fetch(`http://www.omdbapi.com/?s=${input.value}&apikey=adf1f2d7`)
+    .then(response => response.json())
+    .then((data) => {
+      results.innerHTML = "";
+      const films = data.Search;
+      films.forEach((film) => {
+        // results.insertAdjacentHTML(POSITION, HTML_CONTENT)
+        results.insertAdjacentHTML('beforeend', `<li>
+          <img src="${film.Poster}" />
+          <p>${film.Title}</p>
+        </li>`);
+      });
+    });
+});
+
+// SIGN UP DEMO
+// const signUp = (event) => {
+//   event.preventDefault()
+//   const emailValue = document.getElementById("email").value
+//   const passwordValue = document.getElementById("password").value
+//   fetch("https://reqres.in/api/register", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ "email": emailValue, "password": passwordValue })
+//   })
+//     .then(response => response.json())
+//     .then((data) => {
+//       console.log(data)
+//     })
+// }
+
+// const form = document.querySelector("#form")
+// form.addEventListener("submit", signUp)
